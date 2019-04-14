269eb587871bb71e66596559c7ca33264afe9d18
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-14 14:15:54 +0100

    Return JSON

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.end('Hello, wurld.');
+  res.json({ data: 'Hello'});
 });
 
 const PORT = process.env.PORT || 3000;
